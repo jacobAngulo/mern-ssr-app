7017f4bb74a4669060862e6717d37df98a9a5f8e
Jacob Angulo <jacobangulo@hotmail.com>
2019-09-22 13:59:58 -0700

    heroku fix pt 456

--- server/index.js
+++ server/index.js
@@ -4,12 +4,12 @@ const path = require("path");
 const server = express();
 
 // Serve static files from the React app
-server.use(express.static(path.join(__dirname, "client/build")));
+server.use(express.static(path.join(__dirname, "/..client/build")));
 
 // The "catchall" handler: for any request that doesn't
 // match one above, send back React's index.html file.
 server.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname + "/client/build/index.html"));
+  res.sendFile(path.join(__dirname + "/../client/build/index.html"));
 });
 
 module.exports = server;
