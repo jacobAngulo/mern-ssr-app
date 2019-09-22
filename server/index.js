9fbc7bb3c495ccd0c95d9f2822429ecd0c599d19
Jacob Angulo <jacobangulo@hotmail.com>
2019-09-22 13:52:36 -0700

    Update index.js

--- server/index.js
+++ server/index.js
@@ -9,7 +9,7 @@ server.use(express.static(path.join(__dirname, "client/build")));
 // The "catchall" handler: for any request that doesn't
 // match one above, send back React's index.html file.
 server.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname + "../client/build/index.html"));
+  res.sendFile(path.join(__dirname + "/../client/build/index.html"));
 });
 
 module.exports = server;
